4f7d3f2ec5b977ad7d3e52e64ea87dda45dd47a9
HastagKc <chaudharykshittiz950@gmail.com>
2023-05-05 08:47:46 +0545

    adding todo work properly

--- lib/Providers/todo_provider.dart
+++ lib/Providers/todo_provider.dart
@@ -1,5 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/model/todo.dart';
 
-class TodoProvider extends StateNotifier {
-  TodoProvider(super._state);
+List<Todo> todos = [
+  Todo(datetime: DateTime.now().toString(), label: 'Watching Movie '),
+];
+
+// creating an instance of the TodoProvider
+
+final todoProvider = StateNotifierProvider<TodoProvider, List<Todo>>(
+    (ref) => TodoProvider(todos));
+
+class TodoProvider extends StateNotifier<List<Todo>> {
+  TodoProvider(super.state);
+
+  void add(Todo todo) {
+    /* 
+    List<int> list1 = [1, 2, 3];
+    List<int> list2 = [4, 5, 6];
+    List<int> combinedList = [...list1, ...list2];
+
+
+    adding listof items to new combined list 
+ */
+    state = [...state, todo];
+  }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:todo_app/Providers/todo_provider.dart';
+import 'package:todo_app/model/todo.dart';
 
 class HomePage extends StatelessWidget {
   final time = DateTime.now();
@@ -9,6 +11,9 @@ class HomePage extends StatelessWidget {
   // final userName = Get.arguments;
   final String userName;
   HomePage({required this.userName});
+
+  // creating textController
+  final textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +23,69 @@ class HomePage extends StatelessWidget {
             : Text('Good Morning $userName'),
       ),
       body: SafeArea(
-        child: Column(),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Consumer(
+            builder: (context, ref, child) {
+              final todos = ref.watch(todoProvider);
+              return Column(
+                children: [
+                  TextFormField(
+                    controller: textController,
+                    onFieldSubmitted: (value) {
+                      // creating instance of the list then added into the list
+                      final newTodo = Todo(
+                        datetime: DateTime.now().toString(),
+                        label: value,
+                      );
+                      ref.read(todoProvider.notifier).add(newTodo);
+
+                      textController.clear();
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: todos.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        print(todos);
+                        return Card(
+                          child: ListTile(
+                            title: Text(
+                              todos[index].label,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            subtitle: Text(todos[index].datetime),
+                            trailing: Container(
+                              width: 99,
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.edit),
+                                    color: Colors.blue,
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.delete),
+                                    color: Colors.red,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
